c3ea0fe757ae30fe23e230e3273697e342785839
Ikillnukes <ikillnukes1@gmail.com>
2018-11-13 16:17:45 +0100

    Añadidas EasyConsole para añadir a lo rapido opciones de menu

--- EHN-Macros/Program.cs
+++ EHN-Macros/Program.cs
@@ -1,6 +1,5 @@
-﻿#define PURGE
+﻿#define PURGE_MP
 
-using EHN_Macros.Properties;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
@@ -9,7 +8,6 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
-using System.Threading;
 
 namespace EHN_Macros
 {
@@ -30,42 +28,67 @@ namespace EHN_Macros
 
         private static void Main(string[] args)
         {
-            //var options = new ChromeOptions();
-            //options.AddUserProfilePreference("download.default_directory", DownloadPath);
+            var menu = new EasyConsole.Menu()
+              .Add("Guardar y borrar MPs de la bandeja de entrada", () => GuardarMPs())
+              .Add("Salir", () => Environment.Exit(0));
 
+            menu.Display();
+
+            Console.Read();
+        }
+
+        private static void GuardarMPs()
+        {
             using (IWebDriver web = new ChromeDriver())
             {
                 //web.Manage().Window.Maximize();
                 web.Navigate().GoToUrl("http://foro.elhacker.net/login.html");
 
-                web.FindElement(By.Name("user")).SendKeys(Resources.User);
-                web.FindElement(By.Name("passwrd")).SendKeys(Resources.Password);
+                // Nos logueamos
+
+                web.FindElement(By.Name("user")).SendKeys("");
+                web.FindElement(By.Name("passwrd")).SendKeys("");
 
                 web.FindElement(By.CssSelector("input[type='submit']")).Click();
 
+                // Vamos a la bandeja de mensajes
+
                 web.Navigate().GoToUrl("https://foro.elhacker.net/pm.html;f=inbox;sort=date;start=0");
+
+                // Buscamos el numero de páginas
 
                 ReadOnlyCollection<IWebElement> elements = web.FindElements(By.CssSelector("a.navPages"));
 
                 int numberOfPages = int.Parse(elements.Last().Text);
 
+                // Establecemos el encoding de la pagina
+
                 Encoding iso = Encoding.GetEncoding("ISO-8859-1");
+
+                // Recorremos de final a principio
 
                 for (int i = numberOfPages; i >= 1; ++i)
                 {
+                    // Empezaremos por la última página
                     web.Navigate().GoToUrl($"https://foro.elhacker.net/pm.html;f=inbox;sort=date;start={(i - 1) * 10}");
 
+                    // Obtendremos el source
                     var source = iso.GetBytes(web.PageSource);
 
+                    // Guardaremos el source en el archivo correspondiente
                     File.WriteAllBytes(Path.Combine(DownloadPath, $"Page{i}.html"), source);
 
-#if PURGE
+#if PURGE_MP
+                    // Si esta directiva de preprocesador está activada, los mps de la página iterada serán borrados
                     try
                     {
+                        // Se pulsa el checkbox de seleccionar todos los mensajes
                         web.FindElement(By.CssSelector(".titlebg .check")).Click();
+
+                        // Se clica el boton de borrar seleccionados
                         web.FindElement(By.Name("del_selected")).Click();
 
-                        // Aceptamos el alert
+                        // Y aceptamos el alert de confirmación
                         IAlert alert = web.SwitchTo().Alert();
                         alert.Accept();
                     }
@@ -73,13 +96,9 @@ namespace EHN_Macros
                     {
                         // Hemos terminado
                     }
-
-                    //Thread.Sleep(1500);
 #endif
                 }
             }
-
-            Console.Read();
         }
     }
 }
